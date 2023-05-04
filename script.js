d26565bc335c7c2773310bad7ed9927ac20c424f
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-05-03 21:51:07 -0400

    added comments

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
+  $(".saveBtn").on("click",function(){
+    var time = $(this).parent().attr("id")
+    var task = $(this).siblings(".description").val()
+    localStorage.setItem(time, task)
+  })
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -14,6 +19,7 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -21,3 +27,7 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
+/// run same function $("time-block")
+// if statement if time is less than 9 or past/present/future = to something make color green.
